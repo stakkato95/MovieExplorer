be854225d9a41e711385a4f4607bbceefb3c0c25
Artsiom Kaliaha <stakkato95@gmail.com>
2017-04-09 21:43:28 +0200

    playing with swift

--- MovieExplorer/MovieExplorer/ViewController.swift
+++ MovieExplorer/MovieExplorer/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var function: (Int) -> Int = {
+        (input: Int) -> Int in
+        print("")
+        print("hello")
+        let constant = 5
+        var finalResult = constant + 10 + input
+        return finalResult
+    }
+    
+    var isUserTypingText = false
+    
+    var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var num = 5
+        
+        var closure = { self.button.currentTitle }
+        
+        print(String(closure()!)!)
+        
+        func callMe(closu: () -> Void) {
+            print(num)
+            closu()
+        }
+        
+        callMe() {
+            print("called")
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
+        //        print("it was called");
+        //        print("it was called")
+        //
+        //        let button = UIButton()
+        //        button.setTitle("text", for: UIControlState.normal)
+        //        var text = button.currentTitle
+        //        text?.append("a")
+        //
+        //        if text != nil {
+        //            text?.append("b")
+        //        }
+        //
+        //        print("awesome \(text ?? "not set") label")
+        //
+        //        var textOne: String? = nil
+        //        if textOne != nil {
+        //            textOne?.append("a")
+        //        }
+        //
+        //        if let textTwo = UIButton().currentTitle {
+        //            print(textTwo)
+        //        }
+        
+        writeText(from: 12, to: 14)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func writeText(from val: Int, to path: Int) -> Int {
+        return 1;
     }
-
-
 }
 
