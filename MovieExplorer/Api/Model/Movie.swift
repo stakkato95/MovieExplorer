88e2102a5bf2577360eece4495ccce4d00d47ec3
Artsiom Kaliaha <stakkato95@gmail.com>
2017-04-15 15:09:27 +0200

    added movie model

--- MovieExplorer/Api/Model/Movie.swift
+++ MovieExplorer/Api/Model/Movie.swift
@@ -9,6 +9,37 @@
 import Foundation
 import ObjectMapper
 
-class Movie: BaseMappable {
+class Movie: Mappable {
     
+    var id: Int?
+
+    var posterPath: String!
+    
+    var overview: String!
+    
+    var title: String!
+    
+    var releaseDate: String!
+    
+    var voteCount: Int!
+    
+    var averageVote: Double!
+    
+    var videoUrl: String?
+    
+    var isFavourite: Bool?
+    
+    required init?(map: Map) {
+        id <- map["id"]
+        posterPath <- map["poster_path"]
+        overview <- map["overview"]
+        title <- map["title"]
+        releaseDate <- map["release_date"]
+        voteCount <- map["vote_count"]
+        averageVote <- map["vote_average"]
+    }
+    
+    func mapping(map: Map) {
+        
+    }
 }
