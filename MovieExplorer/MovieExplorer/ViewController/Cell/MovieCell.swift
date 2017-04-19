1b39e6012cc5ff475783691ed244ef4fa1391108
Artsiom Kaliaha <stakkato95@gmail.com>
2017-04-19 18:51:23 +0200

    work on layout in progress

---
+++ MovieExplorer/MovieExplorer/ViewController/Cell/MovieCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCell.swift
+//  MovieExplorer
+//
+//  Created by Artsiom Kaliaha on 4/19/17.
+//  Copyright © 2017 Artsiom Kaliaha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    let cellId = "cell"
+    
+    func setData(movie: Movie) {
+        
+    }
+}
